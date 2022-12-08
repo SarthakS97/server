319018699ca95613570fe17356f677db64cf5094
Sarthak <sarthak1509@gmail.com>
2022-12-08 19:56:11 +0530

    forth commit

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json())
 
 
 app.post('https://busy-pear-centipede-wear.cyclic.app/paid', (req, res) => {
     try {
-        res.json(result)
+        console.log(req.body)
     } catch (error) {
         console.log(error)
     }
@@ -17,5 +17,15 @@ app.post('https://busy-pear-centipede-wear.cyclic.app/paid', (req, res) => {
   app.listen(3000, function(){
     console.log("Server is at 3000")
   })
+  app.post('https://bored-jumpsuit-cod.cyclic.app/paid', async (req, res) => {
+	const paid = true
 
+	try {
+		res.json({
+			paid
+		})
+	} catch (error) {
+		console.log(error)
+	}
+})
   
